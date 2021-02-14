953fd745058256722543738b6ac525ccd5c0c720
HanlonsStraightRazor <muellermartin98@gmail.com>
2021-02-14 15:39:17 -0600

    Fixed IdExp and symbol table issues.

--- Project0/src/Interpreter.java
+++ Project0/src/Interpreter.java
@@ -2,11 +2,11 @@ package Starter;
 import java.util.HashMap;
 
 /*
-	Add methods to handle the traversal of other nodes in 
-	the syntax tree. Some methods will need to be updated. 
-	For instance, the interpret method for a Stmt assumes 
-	that all statements are print statements. This is 
-	obviously not the case and needs to be handled.
+  Add methods to handle the traversal of other nodes in 
+  the syntax tree. Some methods will need to be updated. 
+  For instance, the interpret method for a Stmt assumes 
+  that all statements are print statements. This is 
+  obviously not the case and needs to be handled.
 */
 
 public class Interpreter{
@@ -20,78 +20,78 @@ public class Interpreter{
   public int interpret(Stmts ss) {
     if (ss == null) {
       return 0;
-    } else if (ss.s instanceof AssignStmt) {
-      AssignStmt as = interpret(ss.s);
-      symbolTable.put(as.id, as.exp);
     } else {
-      interpret(ss.ss);
+      interpret(ss.s);
+      if (ss.ss != null) {
+        interpret(ss.ss);
+      }
     }
   }
-	//currently assumes all Stmt are PrintStmt
-	//probably needs to be updated
- 	public int interpret(Stmt stm) {
-	   if (stm instanceof PrintStmt)
-    		return this.interpret((PrintStmt)stm);
-		else if (stm instanceof AssignStmt)
-			return this.interpret((AssignStmt)stm);
-		return 0;
- 	}
+  //currently assumes all Stmt are PrintStmt
+  //probably needs to be updated
+  public int interpret(Stmt stm) {
+     if (stm instanceof PrintStmt)
+        return interpret((PrintStmt)stm);
+    else if (stm instanceof AssignStmt)
+      symbolTable.put(interpret(((AssignStmt)stm).id), interpret(((AssignStmt)stm).exp));
+    return 0;
+  }
 
-	//each PrintStmt contains an ExpList
-	//evaluate the ExpList
- 	public int interpret(PrintStmt stm) {
- 		ExpList exp = stm.exps;
- 	   	System.out.println(this.interpret(exp));
- 	   	return 0;
- 	}
+  //each PrintStmt contains an ExpList
+  //evaluate the ExpList
+  public int interpret(PrintStmt stm) {
+    ExpList exp = stm.exps;
+      System.out.println(interpret(exp));
+      return 0;
+  }
 
- 	public int interpret(Expression exp) {
-    	if (exp instanceof NumExp)
-      		return this.interpret((NumExp)exp);
-    	else if (exp instanceof IdExp)
-      		return this.interpret((IdExp)exp);
-    	else if (exp instanceof BinOpExp)
-      		return this.interpret((BinOpExp)exp);
-    	else if (exp instanceof UnaryOpExp)
-      		return this.interpret((UnaryOpExp)exp);
-    	return 0;
- 	}
+  public int interpret(Expression exp) {
+      if (exp instanceof NumExp)
+          return interpret((NumExp)exp);
+      else if (exp instanceof IdExp)
+          return interpret((IdExp)exp);
+      else if (exp instanceof BinOpExp)
+          return interpret((BinOpExp)exp);
+      else if (exp instanceof UnaryOpExp)
+          return interpret((UnaryOpExp)exp);
+      return 0;
+  }
 
- 	public int interpret(NumExp exp) {
-    	return exp.num;
- 	}
+  public int interpret(NumExp exp) {
+      return exp.num;
+  }
 
- 	public String interpret(IdExp exp) {
-    	return exp.id;
- 	}
+  public String interpret(IdExp exp) {
+      return exp.id;
+  }
 
-	public int interpret(BinOpExp exp){
-    	if (exp.binOp == '+')
-        	return interpret(exp.firstExp) + interpret(exp.secondExp);
-    	else if (exp.binOp == '-')
-        	return interpret(exp.firstExp) - interpret(exp.secondExp);
+  public int interpret(BinOpExp exp){
+      if (exp.binOp == '+')
+          return interpret(exp.firstExp) + interpret(exp.secondExp);
+      else if (exp.binOp == '-')
+          return interpret(exp.firstExp) - interpret(exp.secondExp);
         else if (exp.binOp == '*')
             return interpret(exp.firstExp) * interpret(exp.secondExp);
         else if (exp.binOp == '/')
             return interpret(exp.firstExp) / interpret(exp.secondExp);
         else if (exp.binOp == '%')
             return interpret(exp.firstExp) % interpret(exp.secondExp);
-		return 0;
-	}
+    return 0;
+  }
 
- 	public int interpret(UnaryOpExp exp) {
-    	if (exp.urnOp.equals(">>"))
-        	return interpret(exp.urnExp) >> 1;
-    	else if (exp.urnOP.equals("<<"))
-        	return interpret(exp.urnExp) << 1;
-		return 0;
- 	}
+  public int interpret(UnaryOpExp exp) {
+      if (exp.urnOp.equals(">>"))
+          return interpret(exp.urnExp) >> 1;
+      else if (exp.urnOP.equals("<<"))
+          return interpret(exp.urnExp) << 1;
+    return 0;
+  }
 
- 	public ExpList interpret(ExpList list) {
-    	return this.interpret((LastExpList)list);
- 	}
+  public ExpList interpret(ExpList list) {
+      return interpret((LastExpList)list);
+  }
 
- 	public Expression interpret(LastExpList list) {
-    	return this.interpret(list.head);
-  	}
+  public Expression interpret(LastExpList list) {
+      return interpret(list.head);
+    }
 }
