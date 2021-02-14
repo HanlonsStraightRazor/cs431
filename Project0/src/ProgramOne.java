8e83daf0e641e9f31732d0b46df0c3e1c4be2b95
pinkben <42687724+pinkben@users.noreply.github.com>
2021-02-14 13:52:53 -0600

    updates to programOne

--- Project0/src/ProgramOne.java
+++ Project0/src/ProgramOne.java
@@ -1,19 +1,19 @@
 package Starter;
 
 public class ProgramOne {
-  private static Stmts program = new Stmts(
-      new PrintStmt(
-        new LastExpList(
-          new AssignStmt(
-            "one",
-            new NumExp(30)
-          )
-        )
-      )
-    );
-  public static void main(String[] args) {
-    Interpreter interpreter = new Interpreter();
-    System.out.println("Evaluating...");
-    interpreter.interpret(program);
-  }
+    private static Stmts program = new Stmts(
+        new PrintStmt(
+      	    new LastExpList(
+        		    new AssignStmt(
+            	    "one",
+            	    new NumExp(30)
+                )
+            )
+        );
+
+    public static void main(String[] args) {
+        Interpreter interpreter = new Interpreter();
+        System.out.println("Evaluating...");
+        interpreter.interpret(program);
+    }
 }
