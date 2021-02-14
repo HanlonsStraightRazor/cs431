185c114b5706804730ffd5cf09292d2b0fc0e2b5
pinkben <42687724+pinkben@users.noreply.github.com>
2021-02-14 13:38:24 -0600

    fixing errors

--- Project0/src/ProgramOne.java
+++ Project0/src/ProgramOne.java
@@ -3,7 +3,7 @@ package Starter;
 public class ProgramOne{
 	//the syntax tree representation of:  echo(34)
 	//You should create separate programs for each tree you create.
-   private static Stmt program = new PrintStmt(new LastExpList(new AssignStmt("one", new LastExpList(new NumExp(30)))));
+   private static Stmt program = new PrintStmt(new LastExpList(new AssignStmt("one", new NumExp(30))));
 
 	public static void main(String[] args) {
 		//Create a new Interpreter Object
