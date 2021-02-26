fe4394aca3f1fd34f42e169243ddfe5f21433f04
HanlonsStraightRazor <muellermartin98@gmail.com>
2021-02-25 20:34:34 -0600

    Fixed spacing.

--- Project1/Proj1.js
+++ Project1/Proj1.js
@@ -1,10 +1,10 @@
 Package Project1;
 
 Helpers
-            digit = ['0'..'9'];
-            lf = 10 | 13; 
-            sp = ' ';
+    digit = ['0'..'9'];
+    lf = 10 | 13; 
+    sp = ' ';
 Tokens
-            if = 'if';
-			whitespace = lf | sp;
-			number = digit+;
+    if = 'if';
+    whitespace = lf | sp;
+    number = digit+;
