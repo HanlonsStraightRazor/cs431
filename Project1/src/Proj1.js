365286779c0e013fda71a54dddbab92533bee778
HanlonsStraightRazor <muellermartin98@gmail.com>
2021-02-25 22:19:16 -0600

    js file okay for now, but need to fix build file. Doesn't play nice with SableCC.

--- Project1/src/Proj1.js
+++ Project1/src/Proj1.js
@@ -5,10 +5,11 @@ Helpers
     digit = ['0'..'9'];
     lf = 10 | 13;
     sp = ' ';
+    any = [0..25];
 Tokens
     if = 'if';
     whitespace = lf | sp;
     number = digit+;
-    real_number = number '.' digit*;
-    comment = '//' .* lf | '/*' .* '*/';
+    real_number = digit* '.' digit*;
+    comment = ('//' any* lf) | ('/*' any* '*/');
     identifier = letter (digit | letter | '_')*;
