e5f15c0745296e8bd4b7b5f5b75b972520b579a2
HanlonsStraightRazor <muellermartin98@gmail.com>
2021-02-25 23:03:28 -0600

    It compiles, but it doesn't work, so that's cool... Edit: I lied. Now it doesn't work, but I'm going to bed anyway.

--- Project1/src/Main.java
+++ Project1/src/Main.java
@@ -13,15 +13,16 @@ public class Main{
     public static void main(String[] args){
         try{
             // Create a lexer instance.
-            String name = args[0].split(".")[0];
-            System.setOut(new PrintStream(new File(name + ".answer")));
-            File program = new File(args[0]);
-            Lexer l = new Lexer(new PushbackReader
-                    (new FileReader(program), 4096));
-            Token t = l.next();
-            while (!t.getText().equals("")){
-                System.out.print("<"+t.toString()+">");
-                t = l.next();
+            for (String arg : args) {
+                String name = arg.split(".")[0];
+                System.setOut(new PrintStream(new File(name + ".answer")));
+                Lexer l = new Lexer(new PushbackReader
+                        (new FileReader(new File(arg)), 4096));
+                Token t = l.next();
+                while (!t.getText().equals("")){
+                    System.out.print("<"+t.toString()+">");
+                    t = l.next();
+                }
             }
         }
         catch(Exception e){ System.out.println(e.getMessage()); }
