84936436eb2d97a8d15681c4fac8560705669663
Isaiah Ley <isaiahley20@gmail.com>
2021-02-28 15:55:31 -0600

    Cool stuff

--- Project1/src/Proj1.js
+++ Project1/src/Proj1.js
@@ -7,39 +7,31 @@ Helpers
     sp = ' ';
     any = [0..25];
 Tokens
-    if = 'if';
-    whitespace = lf | sp;
-    INTEGER_LITERAL = digit+;
-    REAL_NUMBERS = (<INTEGER_LITERAL>)+ '.' (<INTEGER_LITERAL>)*;
-    comment = ('//' any* lf) | ('/*' any* '*/');
-    identifier = letter (digit | letter | '_')*;
-
-    Goal =	MainClass ( ClassDeclaration )* <EOF>;
-    MainClass = "class" Identifier "{" "public" "static" "void" "main" "(" "String" "[" "]" Identifier ")" "{" Statement "}" "}";
-    ClassDeclaration = "class" Identifier ( "extends" Identifier )? "{" ( VarDeclaration )* ( MethodDeclaration )* "}";
-    VarDeclaration = Type Identifier ";";
-    MethodDeclaration = "public" Type Identifier "(" ( Type Identifier ( "," Type Identifier )* )? ")" "{" ( VarDeclaration )* ( Statement )* "return" Expression ";" "}";
-    Type = "int" "[" "]"
-        | "boolean"
-        | "int"
-        | Identifier;
-    Statement = "{" ( Statement )* "}"
-        | "if" "(" Expression ")" Statement "else" Statement
-        | "while" "(" Expression ")" Statement
-        | "System.out.println" "(" Expression ")" ";"
-        | Identifier "=" Expression ";"
-        |Identifier "[" Expression "]" "=" Expression ";";
-    Expression = Expression ( "&&" | "<" | "+" | "-" | "*" ) Expression
-        | Expression "[" Expression "]"
-        | Expression "." "length"
-        | Expression "." Identifier "(" ( Expression ( "," Expression )* )? ")"
-        | <INTEGER_LITERAL>
-        | "true"
-        | "false"
-        | Identifier
-        | "this"
-        | "new" "int" "[" Expression "]"
-        | "new" Identifier "(" ")"
-        | "!" Expression
-        | "(" Expression ")";
-    Identifier = <IDENTIFIER></IDENTIFIER>;
+    whitespace = (lf | sp);
+    TNumber = (integer_literal)+ '.' (integer_literal)*;
+    integer_literal = digit+;
+    TComments = ('//' any* lf) | ('/*' any* '*/');
+    TID = letter (digit | letter | '_')*;
+    TClass = 'class';
+    TPublic = 'public';
+    TStatic = 'static';
+    TVoid = 'void';
+    TMain = 'main';
+    TThis = 'this';
+    TReturn = 'return';
+    TIf = 'if';
+    TElse = 'else';
+    TAdd = '+';
+    TMinus = '-';
+    TTimes = '*';
+    TLparen = '(';
+    TRparen = ')';
+    TLbracket = '[';
+    TRbracket = ']';
+    TLcurly = '{';
+    TRcurly = '}';
+    TPrint = 'System.out.println';
+    TNew = 'new';
+    TPeriod = '.';
+    TSemicolon = ';';
+    TEqual = '=';
