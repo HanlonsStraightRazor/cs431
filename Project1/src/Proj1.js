abc07a9db3ae3885be312a52df5fd4eaa4b4c52e
Isaiah Ley <isaiahley20@gmail.com>
2021-02-28 13:55:54 -0600

    Added the BNF from the link (DOES NOT WORK)
    DOES NOT WORK

--- Project1/src/Proj1.js
+++ Project1/src/Proj1.js
@@ -9,7 +9,37 @@ Helpers
 Tokens
     if = 'if';
     whitespace = lf | sp;
-    number = digit+;
-    real_number = digit* '.' digit*;
+    INTEGER_LITERAL = digit+;
+    REAL_NUMBERS = (<INTEGER_LITERAL>)+ '.' (<INTEGER_LITERAL>)*;
     comment = ('//' any* lf) | ('/*' any* '*/');
     identifier = letter (digit | letter | '_')*;
+
+    Goal =	MainClass ( ClassDeclaration )* <EOF>;
+    MainClass = "class" Identifier "{" "public" "static" "void" "main" "(" "String" "[" "]" Identifier ")" "{" Statement "}" "}";
+    ClassDeclaration = "class" Identifier ( "extends" Identifier )? "{" ( VarDeclaration )* ( MethodDeclaration )* "}";
+    VarDeclaration = Type Identifier ";";
+    MethodDeclaration = "public" Type Identifier "(" ( Type Identifier ( "," Type Identifier )* )? ")" "{" ( VarDeclaration )* ( Statement )* "return" Expression ";" "}";
+    Type = "int" "[" "]"
+        | "boolean"
+        | "int"
+        | Identifier;
+    Statement = "{" ( Statement )* "}"
+        | "if" "(" Expression ")" Statement "else" Statement
+        | "while" "(" Expression ")" Statement
+        | "System.out.println" "(" Expression ")" ";"
+        | Identifier "=" Expression ";"
+        |Identifier "[" Expression "]" "=" Expression ";";
+    Expression = Expression ( "&&" | "<" | "+" | "-" | "*" ) Expression
+        | Expression "[" Expression "]"
+        | Expression "." "length"
+        | Expression "." Identifier "(" ( Expression ( "," Expression )* )? ")"
+        | <INTEGER_LITERAL>
+        | "true"
+        | "false"
+        | Identifier
+        | "this"
+        | "new" "int" "[" Expression "]"
+        | "new" Identifier "(" ")"
+        | "!" Expression
+        | "(" Expression ")";
+    Identifier = <IDENTIFIER></IDENTIFIER>;
