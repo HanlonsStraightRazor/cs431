a3bf642eac2d9c373464c996f2bb1190bcf433f6
HanlonsStraightRazor <muellermartin98@gmail.com>
2021-03-29 17:57:42 -0500

    Removed duplicate files.

--- Project3/src/ParserProject/ParserGrammar.js
+++ Project3/src/ParserProject/ParserGrammar.js
@@ -1,15 +1,61 @@
 Package ParserProject;
 
 Helpers
-    sp  = ' ' | 10;
+    lf = 10 | 12 | 13;
+    sp = ' ';
+    tabs = 9 | 11;
     number = ['0'..'9'];
     letter = ['a'..'z'] | ['A'..'Z'];
     alphanumeric = letter | number;
 
 Tokens
+    class = 'CLASS';
+    begin = 'BEGIN';
+    end = 'END';
+    new = 'NEW';
+    return = 'RETURN';
+    if = 'IF';
+    then = 'THEN';
+    else = 'ELSE';
+    while = 'WHILE';
+    for = 'FOR';
+    bool = 'BOOLEAN';
+    true = 'TRUE';
+    false = 'FALSE';
+    string = 'STRING';
+    int = 'INT';
+    void = 'VOID';
+    real = 'REAL';
+    switch = 'SWITCH';
+    case = 'CASE';
+    break = 'BREAK';
+    default = 'DEFAULT';
+    get = 'GET';
+    put = 'PUT';
+    plus = '+';
+    minus = '-';
+    times = '*';
+    lparen = '(';
+    rparen = ')';
+    lbracket = '[';
+    rbracket = ']';
+    lcurly = '{';
+    rcurly = '}';
+    lt = '<';
+    gt = '>';
+    lte = '<=';
+    gte = '>=';
+    comma = ',';
+    not = '!';
+    and = '&&';
+    semicolon = ';';
+    equal = '=';
+    eq = '==';
+    neq = '!=';
     id = letter (letter | number)*;
     digit = number+;
-    whitespace = sp+;
+    whitespace = (lf | sp | tabs);
+
 Ignored Tokens
     whitespace;
 
