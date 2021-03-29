9d732b3085d9b268ff2bb996a468a3f629aa1554
Isaiah Ley <isaiahley20@gmail.com>
2021-03-29 17:55:15 -0500

    fixed spacing

--- Project3/ParserGrammar.js
+++ Project3/ParserGrammar.js
@@ -71,29 +71,61 @@ Productions
 
     Prog = BEGIN ClassMethodStmts END;
     ClassMethodStmts = ClassMethodStmts ClassMethodStmt |lambda;
-    ClassMethodStmt = CLASS Id{MethodStmtSeqs}| Type Id(VarList){StmtSeq}|Id(,Id)∗:Type Semi;
+    ClassMethodStmt = CLASS Id{MethodStmtSeqs}|
+        Type Id(VarList){StmtSeq}|
+        Id(,Id)∗:Type semicolon;
     MethodStmtSeqs = MethodStmtSeqs MethodStmtSeq|lambda;
-    MethodStmtSeq = Type Id(VarList){StmtSeq}|Id(,Id)∗:Type Semi;
+    MethodStmtSeq = Type Id(VarList){StmtSeq}|
+        Id(,Id)∗:Type semicolon;
     StmtSeq = Stmt StmtSeq|lambda;
-    Stmt = Id( [Int] )? :=Expr Semi|Id( [Int] )? := “AnyChars” Semi|Id(,Id)∗:Type( [Int] )? Semi|IF (Boolean) THEN{StmtSeq}|IF (Boolean) 
-        THEN{StmtSeq}ELSE{StmtSeq}|WHILE (Boolean){StmtSeq}|FOR ( (Type)?Id:=Expr Semi Boolean Semi (Id++|Id−− |Id:=Expr) ){StmtSeq}|
-        Id( [Int] )? := GET() Semi |PUT (Id( [Int] )? ) Semi |Id( [Int] )?++ Semi |Id( [Int] )?−− Semi |Id( [Int] )? := NEW Id() Semi |
-        Id(VarListTwo) Semi |Id( [Int] )? .Id (VarListTwo) ( .Id (VarListTwo) )∗ Semi | RETURN Expr Semi 1
-        |Id( [Int] )? :=Boolean Semi |SWITCH (Expr){CASE(Int) :StmtSeq(BREAK Semi)? ( CASE(Int) :StmtSeq(BREAK Semi)?)∗DEFAULT :StmtSeq};
+    Stmt = Id( [Int] )? :=Expr semicolon|
+        Id( [Int] )? := “AnyChars” semicolon|
+        Id(,Id)∗:Type( [Int] )? semicolon|
+        IF (Boolean) THEN{StmtSeq}|
+        IF (Boolean) THEN{StmtSeq}ELSE{StmtSeq}|
+        WHILE (Boolean){StmtSeq}|
+        FOR ( (Type)?Id:=Expr semicolon Boolean Semi (Id++|Id−− |Id:=Expr) ){StmtSeq}|
+        Id( [Int] )? := GET() semicolon |
+        PUT (Id( [Int] )? ) semicolon |
+        Id( [Int] )?++ semicolon |
+        Id( [Int] )?−− semicolon |
+        Id( [Int] )? := NEW Id() Semi |
+        Id(VarListTwo) semicolon |
+        Id( [Int] )? .Id (VarListTwo) ( .Id (VarListTwo) )∗ semicolon | RETURN Expr semicolon |
+        Id( [Int] )? :=Boolean semicolon |
+        SWITCH (Expr){CASE(Int) :StmtSeq(BREAK semicolon)? ( CASE(Int) :StmtSeq(BREAK semicolon)?)∗DEFAULT :StmtSeq};
     VarList = (Id:Type( [Int] )? (,Id:Type( [Int] )? )∗)?;
     VarListTwo = (Expr(,Expr)∗)?;
     Expr = Expr AddOp Term|Term;
     Term = Term MultOp Factor|Factor;
-    Factor = (Expr)|-Factor|Int|Real|Boolean|Id( [Int] )?|Id(VarListTwo)|Id( [Int] )? .Id(VarListTwo);
+    Factor = (Expr)|
+        -Factor|
+        Int|
+        Real|
+        Boolean|
+        Id( [Int] )?|
+        Id(VarListTwo)|
+        Id( [Int] )? .Id(VarListTwo);
     Boolean = TRUE|FALSE|Expr Cond Expr|Id;
-    Cond = ==|!=|>=|<=|>|<;
-    AddOp = +|-;
-    MultOp = *|/;
-    Type = INT|REAL|STRING|BOOLEAN|VOID|Id;
+    Cond = ==|
+        !=|
+        >=|
+        <=|
+        >|
+        <;
+    AddOp = +|
+        -;
+    MultOp = *|
+        /;
+    Type = INT|
+        REAL|
+        STRING|
+        BOOLEAN|
+        VOID|
+        Id;
     Id = Letter(Letter|Digit|)∗2;
     Int = Digit(Digit)∗;
     Real = (Digit)+.(Digit)+;
     AnyChars = [35..255]+;
     Letter = [‘a’..‘z’]|[‘A’..‘Z’];
     Digit = [‘0’..‘9’];
-    Semi = ';';
