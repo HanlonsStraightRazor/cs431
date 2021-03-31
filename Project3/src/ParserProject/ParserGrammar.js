98fab5abb651cc9df7ce37fc819d74fcbc8d3f80
HanlonsStraightRazor <muellermartin98@gmail.com>
2021-03-30 20:54:22 -0500

    Fixed 'int' ambiguity.

--- Project3/src/ParserProject/ParserGrammar.js
+++ Project3/src/ParserProject/ParserGrammar.js
@@ -60,7 +60,7 @@ Tokens
     id = letter ( letter | digit | '_' )*;
     whitespace = (lf | sp | tabs);
     anychars = [35..255]+;
-    int = digit ( digit )*;
+    integer = digit ( digit )*;
     real = ( digit )+ '.' ( digit )+;
     cond = '==' | '!=' | '>=' | '<=' | '>' | '<';
     addop = '+' | '-';
@@ -73,7 +73,7 @@ Ignored Tokens
 Productions
     comma_id_star = comma id comma_id_star
         | ;
-    lbracket_int_rbracket_question = lbracket int rbracket
+    lbracket_integer_rbracket_question = lbracket integer rbracket
         | ;
     type_question = type
         | ;
@@ -84,11 +84,13 @@ Productions
     idinc_iddec_idwalrusexpr = id inc
         | id dec
         | id walrus expr;
-    case_lparen_int_rparen_colon_stmtseq = case lparen int rparen colon stmtseq case_lparen_int_rparen_colon_stmtseq
+    case_lparen_integer_rparen_colon_stmtseq = case lparen integer rparen colon stmtseq case_lparen_integer_rparen_colon_stmtseq
         | ;
     comma_expr_star = comma expr comma_expr_star
         | ;
-    comma_id_colon_type_lbracketintrbracketquestion_star = comma id colon type lbracket_int_rbracket_question comma_id_colon_type_lbracketintrbracketquestion_star
+    comma_id_colon_type_lbracketintegerrbracketquestion_star = comma id colon type lbracket_integer_rbracket_question comma_id_colon_type_lbracketintegerrbracketquestion_star
+        | ;
+    id_colon_type_lbracketintegerrbracketquestion_commaidcolontypelbracketintegerrbracketquestionstar_question = id colon type lbracket_integer_rbracket_question comma_id_colon_type_lbracketintegerrbracketquestion_star
         | ;
 
     prog = begin classmethodstmts end;
@@ -103,39 +105,37 @@ Productions
         | id commaid_star colon type semicolon;
     stmtseq = stmt stmtseq
         | ;
-    stmt = id lbracket_int_rbracket_question walrus expr semicolon
-        | id lbracket_int_rbracket_question walrus lquote anychars rquote semicolon
-        | id comma_id_star colon type lbracket_int_rbracket_question semicolon
+    stmt = id lbracket_integer_rbracket_question walrus expr semicolon
+        | id lbracket_integer_rbracket_question walrus lquote anychars rquote semicolon
+        | id comma_id_star colon type lbracket_integer_rbracket_question semicolon
         | if lparen boolean rparen then lcurly stmtseq rcurly
         | if lparen boolean rparen then lcurly stmtseq rcurly else lcurly stmtseq rcurly
         | while lparen boolean rparen lcurly stmtseq rcurly
         | for lparen type_question id walrus expr semicolon boolean semicolon idinc_iddec_idwalrusexpr rparen lcurly stmtseq rcurly
-        | id lbracket_int_rbracket_question walrus get lparen rparen semicolon
-        | put lparen id lbracket_int_rbracket_question rparen semicolon
-        | id lbracket_int_rbracket_question inc semicolon
-        | id lbracket_int_rbracket_question dec semicolon
-        | id lbracket_int_rbracket_question walrus new id lparenrparen semicolon
+        | id lbracket_integer_rbracket_question walrus get lparen rparen semicolon
+        | put lparen id lbracket_integer_rbracket_question rparen semicolon
+        | id lbracket_integer_rbracket_question inc semicolon
+        | id lbracket_integer_rbracket_question dec semicolon
+        | id lbracket_integer_rbracket_question walrus new id lparenrparen semicolon
         | id lparen varlisttwo rparen semicolon
-        | id lbracket_int_rbracket_question dot id lparen varlisttwo rparen dot_id_lparen_varlisttwo_rparen_star semicolon
+        | id lbracket_integer_rbracket_question dot id lparen varlisttwo rparen dot_id_lparen_varlisttwo_rparen_star semicolon
         | return expr semicolon
-        | id lbracket_int_rbracket_question walrus boolean semicolon
-        | switch lparen expr rparen lcurly case lparen int rparen colon stmtseq break_semicolon_question case_lparen_int_rparen_colon_stmtseq default colon stmtseq rcurly;
-    varlist = id colon type lbracket_int_rbracket_question comma_id_colon_type_lbracketintrbracketquestion_star
-        | ;
-    varlisttwo = expr comma_expr_star
-        | ;
+        | id lbracket_integer_rbracket_question walrus boolean semicolon
+        | switch lparen expr rparen lcurly case lparen integer rparen colon stmtseq break_semicolon_question case_lparen_integer_rparen_colon_stmtseq default colon stmtseq rcurly;
+    varlist = id_colon_type_lbracketintegerrbracketquestion_commaidcolontypelbracketintegerrbracketquestionstar_question;
+    varlisttwo = expr_commaexprstar_question;
     expr = expr addop term
         | term;
     term = term multop factor
         | factor;
     factor = lparen expr rparen
         | minus factor
-        | int
+        | integer
         | real
         | boolean
-        | id lbracket_int_rbracket_question
+        | id lbracket_integer_rbracket_question
         | id lparen varlisttwo rparen
-        | id lbracket_int_rbracket_question dot id lparen varlisttwo rparen;
+        | id lbracket_integer_rbracket_question dot id lparen varlisttwo rparen;
     boolean = true
         | false
         | expr cond expr
