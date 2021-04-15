b26a8babe7f2254f64226fb8ec2414eab21a84d0
HanlonsStraightRazor <muellermartin98@gmail.com>
2021-04-15 16:15:17 -0500

    Added Variable object and corresponding enumerated types.

---
+++ Project4/src/Variable.java
@@ -0,0 +1,26 @@
+package FinalProject;
+
+class enum Type {
+    INT, REAL, STRING, BOOLEAN, VOID
+}
+
+class Variable {
+    private Type type;
+    private Object value;
+    public Variable(Type type, Object value) {
+        this.type = type;
+        this.value = value;
+    }
+    public Type getType() {
+        return type;
+    }
+    public void setType(Type type) {
+        this.type = type;
+    }
+    public Object getValue() {
+        return value;
+    }
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
