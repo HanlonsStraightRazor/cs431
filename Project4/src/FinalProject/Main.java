0ef2585338affd2ac2eabeb08097a7e30e371e74
HanlonsStraightRazor <muellermartin98@gmail.com>
2021-04-19 19:17:51 -0500

    Cleaned up Main class. It was also missing a curly brace.

--- Project4/src/FinalProject/Main.java
+++ Project4/src/FinalProject/Main.java
@@ -7,13 +7,14 @@ import java.io.*;
 
 public class Main{
     public static void main(String[] arguments){
-        try{
+        try {
             Lexer lexer = new Lexer(new PushbackReader
                     (new InputStreamReader(System.in), 1024));
             Parser parser = new Parser(lexer);
             Start ast = parser.parse();
             ast.apply(new PrintTree());  //this is what gets the depth first search going
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
-        catch(Exception e){ System.out.println("Error: " + e.getMessage()); }
     }
-
+}
