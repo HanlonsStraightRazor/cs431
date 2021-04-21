01a477861c7829160f68fa0e57fe3c3fd99e6c12
Isaiah Ley <isaiahley20@gmail.com>
2021-04-21 18:00:50 -0500

    removed starter code

--- Project4/src/FinalProject/PrintTree.java
+++ Project4/src/FinalProject/PrintTree.java
@@ -14,32 +14,6 @@ class PrintTree extends DepthFirstAdapter {
     public PrintTree() {
         System.out.println("Start of the Printing Action");
     }
-    /*
-    //this gets called if the production is prog --> id digit
-    public void caseAFirstProg(AFirstProg node){
-        System.out.println("\tGot a first prog!");
-    }
-
-    //prog --> lotnumbers
-    public void caseASecondProg(ASecondProg node){
-        System.out.println("\tGot a second prog!");
-        node.getLotnumbers().apply(this);
-    }
-
-    //prog --> id id digit digit
-    public void caseAThirdProg(AThirdProg node){
-        System.out.println("\tGot a third prog!");
-        node.getEachsymbolisuniqueinaproduction().apply(this);
-        node.getSecondid().apply(this);
-        node.getDigitone().apply(this);
-        node.getDigittwo().apply(this);
-    }
-
-    //if it reaches an id, print it off
-    public void caseTId(TId node){
-        System.out.println("\tGot myself an id: <"+node.getText()+">");
-    }
-    */
 
     @Override
     public void caseStart(Start node) {
@@ -1266,11 +1240,4 @@ class PrintTree extends DepthFirstAdapter {
             node.getId().apply(this);
         }
     }
-
-    /*
-    //if it reaches a digit, print it off
-    public void caseTDigit(TDigit node){
-        System.out.println("\tGot myself a digit: <"+node.getText()+">");
-    }
-    */
 }
