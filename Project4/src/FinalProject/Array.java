c9761712fb7532837a4c7826c3739e2bdfccb909
HanlonsStraightRazor <muellermartin98@gmail.com>
2021-04-25 15:05:54 -0500

    Removed ability to store values in Variables and Arrays. Instead, there are booleans keeping track of initialization.

--- Project4/src/FinalProject/Array.java
+++ Project4/src/FinalProject/Array.java
@@ -2,26 +2,26 @@ package FinalProject;
 
 class Array extends Symbol {
     private String type;
-    private Object[] values;
     private int size;
+    public boolean[] initialized;
     public Array(String type, int size) {
         this.type = type;
-        this.values = new Object[size];
         this.size = size;
+        initialized = new boolean[size];
+        for (boolean b : initialized) {
+            b = false;
+        }
     }
     public String getType() {
         return type;
     }
-    public Object[] getValues() {
-        return values;
-    }
-    public Object getValueAt(int index) {
-        return values[index];
-    }
-    public void setValueAt(int index, Object value) {
-        values[index] = value;
-    }
     public int getSize() {
         return size;
+    }
+    public boolean isInitializedAt(int index) {
+        return initialized[index];
+    }
+    public void initializeAt(int index) {
+        initialized[index] = true;
     }
 }
--- Project4/src/FinalProject/Variable.java
+++ Project4/src/FinalProject/Variable.java
@@ -2,23 +2,23 @@ package FinalProject;
 
 class Variable extends Symbol {
     private String type;
-    private Object value;
     private int offset;
+    private boolean initialized;
     public Variable(String type, int offset) {
         this.type = type;
-        this.value = null;
         this.offset = offset;
+        initialized = false;
     }
     public String getType() {
         return type;
     }
-    public Object getValue() {
-        return value;
-    }
-    public void setValue(Object value) {
-        this.value = value;
-    }
     public int getOffset() {
         return offset;
+    }
+    public boolean isInitialized() {
+        return initialized;
+    }
+    public void initialize() {
+        initialized = true;
     }
 }
